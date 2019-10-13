d2647fabff38a4c3f73b75bbcbc785b24d574a29
administrator <amintado@gmail.com>
2019-10-13 19:19:15 +0330

    debug tls errors

--- net/net-events.c
+++ net/net-events.c
@@ -1,15 +1,19 @@
 /*
     This file is part of Mtproto-proxy Library.
+
     Mtproto-proxy Library is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 2 of the License, or
     (at your option) any later version.
+
     Mtproto-proxy Library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Lesser General Public License for more details.
+
     You should have received a copy of the GNU Lesser General Public License
     along with Mtproto-proxy Library.  If not, see <http://www.gnu.org/licenses/>.
+
     Copyright 2009-2013 Vkontakte Ltd
               2008-2013 Nikolai Durov
               2008-2013 Andrey Lopatin
